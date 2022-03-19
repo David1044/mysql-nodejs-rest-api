3e65922b4a381c17bb95fb89a78e0e4f4411394c
Davicho HF <39805786+David1044@users.noreply.github.com>
2022-03-18 19:53:01 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'company',
-  multipleStatements: true
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'tecate01',
+    database: 'fortaweb'
 });
 
 mysqlConnection.connect(function (err) {
-  if (err) {
-    console.error(err);
-    return;
-  } else {
-    console.log('db is connected');
-  }
+    if(err) {
+        console.log(err);
+        return;
+    } else {
+        console.log('Db conected');
+    }
+    
 });
 
 module.exports = mysqlConnection;
